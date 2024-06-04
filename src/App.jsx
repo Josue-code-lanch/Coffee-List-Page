276139bbea9b43f414ae6fb2b69513603ac9408a
Josue Gonzalez <josuegr834@gmail.com>
2024-06-04 15:29:14 -0300

    Moviendo proyecto a un repo individual

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Container from "./Components/Container";
+import CoffeeMenu from "./Components/CoffeeMenu";
+
+function App() {
+  return (
+    <>
+      <div className="contain">
+        <Container />
+        <CoffeeMenu />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/CoffeeMenu.jsx
@@ -0,0 +1,98 @@
+import Star from "./Star";
+import StarFill from "./StarFill";
+import Votes from "./Votes";
+import "../Styles/CoffeeMenu.css";
+
+export default function CoffeMenu() {
+  const pop = "Popular";
+  return (
+    <>
+      <div className="row">
+        <Card
+          popular={pop}
+          img={"coffee1.jpg"}
+          coffee={"Cappuccino"}
+          price={"$5.20"}
+          votes={"4.7 (65 votes)"}
+        />
+        <Card
+          popular={pop}
+          img={"coffee2.jpg"}
+          coffee={"Hosue Coffee"}
+          price={"$3.50"}
+          votes={"4.85 (14 votes)"}
+        />
+        <Card
+          img={"coffee3.jpg"}
+          coffee={"Espresso"}
+          price={"$2.50"}
+          votes={"4.9 (55 votes)"}
+        />
+      </div>
+      <div className="row">
+        <Card
+          img={"coffee4.jpg"}
+          coffee={"Coffee Latte"}
+          price={"$4.50"}
+          votes={"5.0 (23 votes)"}
+        />
+        <Card
+          img={"coffee5.jpg"}
+          coffee={"Chocolate Coffee"}
+          price={"$4.00"}
+          votes={"4.65 (122 votes)"}
+          soldOut={"Sold Out"}
+        />
+        <CardEnd
+          img={"coffee6.jpg"}
+          coffee={"Valentine Special"}
+          price={"$5.50"}
+          votes={"No ratings"}
+        />
+      </div>
+    </>
+  );
+}
+
+function Card({ img, coffee, price, votes, soldOut }) {
+  return (
+    <div className="card">
+      <img src={img} alt={coffee}></img>
+      <div className="content">
+        <div className="details">
+          <p className="coffee">{coffee}</p>
+          <div className="vots">
+            <Star />
+            <p className="vote">{votes}</p>
+          </div>
+        </div>
+        <div className="votes">
+          <p className="price">{price}</p>
+          <p className="soldOut">{soldOut}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function CardEnd({ img, coffee, price, votes }) {
+  return (
+    <>
+      <div className="card">
+        <img src={img} alt={coffee}></img>
+        <div className="content">
+          <div className="details">
+            <p className="coffee">{coffee}</p>
+            <div className="vots">
+              <StarFill />
+              <p className="vote">{votes}</p>
+            </div>
+          </div>
+          <div className="votes">
+            <p className="price">{price}</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
